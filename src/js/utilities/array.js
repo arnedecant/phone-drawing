e9fab6a238103eff2949f6f11a37eb7a8e040dce
Arne Decant <hello@arnedecant.be>
2022-02-19 10:32:39 +0100

    re-init with vue 3

--- src/js/utilities/array.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import { random } from './math'
-
-export const getArrayWithNoise = (array, noise) => {
-    return array.map(item => item + random(-noise, noise));
-}
-
-Array.prototype.noise = () => getArrayWithNoise(this)
--- src/js/utilities/string.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-export const capitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-}
-
-String.prototype.capitalize = () => capitalize(this)
